5b275f5bf0d4e14cf5ca52df5444fd2d52f53e32
Kevin Berg <kevinberg@Kevins-MacBook-Pro.local>
2015-10-13 08:57:18 -0700

    Convert to use Makefile

--- features/steps/guineapigsteps.py
+++ features/steps/guineapigsteps.py
@@ -3,10 +3,6 @@ from pages.sauceguineapigmain import SauceGuineaPig
 from pages.sauceguineapig2 import SauceGuineaPig2
 from features.browser.browsers import make_browser
 
-@given('we are also on "{platform}" using "{browser_name}" version "{version}"')
-def step_impl(context, platform, browser_name, version ):
-  context.browser = make_browser(context.name, platform, browser_name, version)
-
 @given('we are looking at the guinea pig website')
 def step_impl(context):
  context.page = SauceGuineaPig(context.browser)
@@ -27,10 +23,6 @@ def step_impl(context):
 def step_impl(context):
   value = context.page.find_email_input().get_attribute('value')
   assert value == 'kevinmarkvi@gmail.com', "The value is incorrect"
-
-@given('we use "{platform}" and "{browser_name}" version "{version}"')
-def step_impl(context, platform, browser_name, version):
-  context.browser = make_browser(context.name, platform, browser_name, version)
 
 @given('we are on the guinea pig website')
 def step_impl(context):
